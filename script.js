7292b58dfe4ad0b7e67e64cb3fe26e001a97b79d
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-02-21 14:58:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var grid =  document.getElementById("theGrid");
+
 function makeButtons(){
+  var btn = document.createElement("button");
+  btn.setAttribute("type", "button");
+  btn.setAttribute("id","one");
+  grid.appendChild(btn);
   
 }//end makeButtons
