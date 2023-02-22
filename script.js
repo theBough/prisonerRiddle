1e2e3ce703289c23845b3887022d2de7c263f832
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-02-22 15:04:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var grid =  document.getElementById("theGrid");
+var randomNumbers = [];
 
 function makeButtons(){
+  fillArray()
   document.getElementById("btnStart").style.visibility = "hidden"
   for(i=0 ; i<100 ; i++){
     var btn = document.createElement("button");
@@ -9,6 +11,10 @@ function makeButtons(){
     grid.appendChild(btn);
     document.getElementById(i).innerHTML = i+1
   }//end loop
-  
-  
 }//end makeButtons
+function fillArray(){
+  var rndNum = Math.random()*100;
+  rndNum = Math.ceil(rndNum);
+  randomNumbers.push(rndNum);
+  console.log(randomNumbers)
+}
