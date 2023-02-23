2603d5862991f993bdd615f47bd1556c044fe624
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-02-23 14:59:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,11 @@ function makeButtons(){
     btn.setAttribute("id",i);
     grid.appendChild(btn);
     document.getElementById(i).innerHTML = i+1
+    document.getElementById(i).onclick = thisClicked;
   }//end loop
 }//end makeButtons
 function fillArray(){
-  while(randomNumbers.length < 99){
+  while(randomNumbers.length < 100){
     var rndNum = Math.random()*100;
     rndNum = Math.ceil(rndNum);
     if(!randomNumbers.includes(rndNum)){
@@ -21,4 +22,8 @@ function fillArray(){
     }
   }//end loop
   console.log(randomNumbers)
+}
+var thisClicked = function(){
+  document.getElementById(this.id).innerHTML = randomNumbers[this.id]
+  document.getElementById(this.id).style.backgroundColor = "#D8F3DC"
 }
